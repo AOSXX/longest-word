766b57c4135e567c2a4f0184cd2c993543a083ba
Jerome Drescig <jeromedrescig@gmail.com>
2022-12-20 11:29:15 +0000

    Feature complete: Dictionary check of attempt

--- longest_word/game.py
+++ longest_word/game.py
@@ -2,7 +2,7 @@
 # pylint: disable=too-few-public-methods
 import random
 import string
-
+import requests
 
 class Game:
     """Game is a class to play the matching word game"""
@@ -13,6 +13,8 @@ class Game:
     def is_valid(self, word: str) -> bool:
         """Return True if and only if the word is valid, given the Game's grid"""
         temp = self.grid
+                                
+
         if not word:
             return False
         for i in word:
@@ -26,4 +28,11 @@ class Game:
                 return False
 
 
-        return True
+        return self.__check_dictionary(word)
+    
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
+        json_response = response.json()
+        return json_response['found']
+
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,6 +1,6 @@
 from longest_word.game import Game
 import string
-
+import requests
 
 class TestGame:
     def test_game_initialization(self):
@@ -32,4 +32,5 @@ class TestGame:
     def test_unknown_word_is_invalid(self):
         new_game = Game()
         new_game.grid = list('KWIENFUQW') # Force the grid to a test case:
+
         assert new_game.is_valid('FEUN') is False
